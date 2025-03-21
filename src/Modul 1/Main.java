d7748158d07e96713c1c7ec4d9c7db419e090f76
nahira08 <nurainistudy08@gmail.com>
2025-03-21 23:02:37 +0700

    praktek modul 1- java util date

---
+++ src/Modul 1/Main.java
@@ -0,0 +1,11 @@
+// Author : Nur Aini
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Current date and time: " + new Date());
+    }
+}
+
